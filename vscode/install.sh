2a75a155f3d6e07b7a5b0a4e11562d5160940de0
Andy Molev <amoliev@amazon.lu>
2019-08-18 20:55:46 +0200

    Updating vscode configuration and keybindings

---
+++ vscode/install.sh
@@ -0,0 +1,4 @@
+# Creates symbolic links for VSCodium configuration files
+ln -sf $HOME/.dotfiles/vscode/keybindings.json $HOME/Library/Application\ Support/VSCodium/User/keybindings.json
+ln -sf $HOME/.dotfiles/vscode/settings.json $HOME/Library/Application\ Support/VSCodium/User/settings.json
+
