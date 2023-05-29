677944212d05d1247430d0bca3c71e411e1d3c95
Andy Molev <amolev@hydrosat.com>
2023-05-30 00:16:38 +0200

    Adjusting configuration of vscode, git, karabiner

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,4 +1,6 @@
 # Creates symbolic links for VSCodium configuration files
 ln -sf $HOME/.dotfiles/vscode/keybindings.json $HOME/Library/Application\ Support/VSCodium/User/keybindings.json
 ln -sf $HOME/.dotfiles/vscode/settings.json $HOME/Library/Application\ Support/VSCodium/User/settings.json
+ln -sf $HOME/.dotfiles/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+ln -sf $HOME/.dotfiles/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
 
